6d6f0438bd2dd4cf2c69859adf467865197191a5
flower48237 <3239349119@qq.com>
2021-11-28 18:23:23 +0800

    LeetCode精选TOP面试题102-补充题目描述

--- interview/src/it102/TreeNode.java
+++ interview/src/it102/TreeNode.java
@@ -1,7 +1,9 @@
 package interview.src.it102;
 
 /**
- * @Description
+ * @Description 102. 二叉树的层序遍历
+ * 给定一个二叉树，返回按 层序遍历 得到的节点值。
+ * 即逐层地，从左到右访问所有节点。相同层的节点值放在一个列表中
  * @Author liq
  * @Date 2021/11/28
  */
