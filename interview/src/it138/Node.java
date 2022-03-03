4c584399e6df74cc33541974015bcff4d06e6c5f
Qiang Li <3239349119@qq.com>
2022-03-03 17:03:28 +0800

    LeetCode精选TOP面试题138.复制带随机指针的链表

---
+++ interview/src/it138/Node.java
@@ -0,0 +1,18 @@
+package interview.src.it138;
+
+/**
+ * @Description
+ * @Author liq
+ * @Date 2022/03/03
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
